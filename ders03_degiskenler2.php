b3ddfa5dd4bdd820fa0519e0871bcce4d88c74ec
Sevgi <sevgi145360@gmail.com>
2025-07-26 20:55:17 +0300

    Add files via upload

---
+++ ders03_degiskenler2.php
@@ -0,0 +1,36 @@
+<?php
+$ad= "Mehmet";
+$soyadi= "Kahraman";
+echo $ad;
+echo $soyadi;
+echo "<hr>";
+echo $ad;
+echo "<br>";
+echo $soyadi;
+echo "<hr>";
+$metin= "Merhaba Ben: $ad $soyadi";
+echo $metin;
+echo "<hr>";
+$metin2= "Merhaba Ben: {$ad} {$soyadi}";
+echo $metin2;
+echo "<hr>";
+$sonuc= $ad.$soyadi;
+$sonuc2= $ad." ".$soyadi;
+echo $sonuc;
+echo "<hr>";
+echo $sonuc2;
+echo "<br>";
+echo "<hr>";
+// Referansa Bagli Deger Atama, Miras Alma
+$a= 5;
+$b= $a;
+echo "A'nın Değeri: {$a}";
+echo "<br>";
+echo "B'nın Değeri: {$b}";
+echo "<br>";
+echo "<hr>";
+$a= 10;
+echo "A'nın Yeni Değeri: {$a}";
+echo "<br>";
+echo "B'nin Yeni Değeri: {$b}";
+?>
